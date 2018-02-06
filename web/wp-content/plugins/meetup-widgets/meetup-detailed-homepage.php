b047be61833d451ff7ddeb897ceb31fb8fca2070
Patrick Connolly <patrick.c.connolly@gmail.com>
2018-02-05 23:01:15 -0500

    Allow meetup widget to take either topic or speaker or both.

--- web/wp-content/plugins/meetup-widgets/meetup-detailed-homepage.php
+++ web/wp-content/plugins/meetup-widgets/meetup-detailed-homepage.php
@@ -6,8 +6,8 @@
         $regex = "/.+ #(?<number>\d+)(?: with (?<speaker>[\w &']+))?(?:: (?<topic>.+))?/";
         preg_match($regex, $event->name, $matches);
         $hacknight_number = $matches['number'];
-        $speaker = $matches['speaker'];
-        $topic = $matches['topic'];
+        $title = isset($matches['topic']) ? $matches['topic'] : $matches['speaker'];
+        $subtitle = isset($matches['speaker']) ? $matches['topic'] : null;
       }
     ?>
 
@@ -29,22 +29,22 @@
 
       foreach ( $events as $event ) {
         $venue = $event->venue->name.' '.$event->venue->address_1 . ', ' . $event->venue->city . ', ' . $event->venue->state;
-        //PATCON: the var here expects meetup event name to end with "with <speaker or topic>"
         $description = substr($event->description, 0, strpos($event->description, "About the Hacknight"));
+        //PATCON: the var here expects meetup event name to end with "with <speaker or topic>"
       	printf(
       		'<h5 class="meetup-eyebrow">Next Civic Tech Toronto Hacknight</h5>
           <h2><div class="calendar-icon"></div>%1$s</h2>
-          <h1 class="meetup-topic" style="margin-bottom: 0;">%6$s</h1>
-          <h3 class="meetup-speaker">%2$s</h3>
-          <div class="meetup-descriptor">%3$s</div>
+          <h1 class="meetup-title" style="margin-bottom: 0;">%2$s</h1>
+          <h3 class="meetup-subtitle">%3$s</h3>
+          <div class="meetup-descriptor">%4$s</div>
           <div class="meetup-rsvp">
-            <a class="btn btn-primary custom-button red-btn" style="margin-right: 20px; margin-left: 0; padding-left: 50px; padding-right: 50px;" href="%4$s">RSVP</a><span class="going-icon"></span><span style="font-weight: 300;">%5$s going</span></div>',
+            <a class="btn btn-primary custom-button red-btn" style="margin-right: 20px; margin-left: 0; padding-left: 50px; padding-right: 50px;" href="%5$s">RSVP</a><span class="going-icon"></span><span style="font-weight: 300;">%6$s going</span></div>',
           date( 'l F d, g:ia', intval( $event->time / 1000 + $event->utc_offset / 1000 ) ),
-          $speaker,
+          $title,
+          $subtitle,
           strip_tags($description, '<p>'),
       		esc_url( $event->event_url ),
-          ($event->yes_rsvp_count),
-          $topic
+          ($event->yes_rsvp_count)
       	);
       }
       ?>
